2fcc3c7001aec9c8c88735dfb6e1d03b2912e448
Anujkumar Singh <anujkumarsingh@air-watch.com>
2015-04-22 20:12:01 +0530

    Checking Json Read

--- src/main/java/org/hp/samples/AddUserServlet.java
+++ src/main/java/org/hp/samples/AddUserServlet.java
@@ -1,11 +1,13 @@
 package org.hp.samples;
 
+import jdk.nashorn.internal.parser.JSONParser;
 import org.hp.samples.Model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,12 +20,24 @@ public class AddUserServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user=new User();
-        user.Name=request.getParameter("Name");
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+        System.out.println(sb.toString());
 
+        user.Name=request.getParameter("Name");
         response.setContentType("text/plain");
         response.setStatus(200);
         PrintWriter writer = response.getWriter();
         writer.println("I am adding user");
+        writer.println(sb.toString());
         writer.close();
     }
 }
